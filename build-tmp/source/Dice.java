4a852fdff78e36643498775ba4ef724890d2b9b2
unknown <tholsen@697-334-Boromir.sf.k12.internal>
2015-09-04 07:35:36 -0700

    dice done

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,10 +1,26 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 
 Die [] dice;
 int flip=0;
 
 int swidth=500;
 int sheight=500;
-void setup()
+public void setup()
 {
   
   background(225);
@@ -13,7 +29,7 @@ void setup()
   size(swidth,sheight);
   dicecreate();
 }
-void draw()
+public void draw()
 {
   background(225);
 int allroll =0;
@@ -29,11 +45,11 @@ int allroll =0;
   text("total roll: " + allroll,200,15);
   
 }
-void mousePressed()
+public void mousePressed()
 {
   redraw();
 }
-void dicecreate(){
+public void dicecreate(){
   dice = new Die[(int)(swidth/7)*(int)(sheight/7)];
   int diecounter = 0;
   for(int p=3;p<(int)(sheight/7);p++){
@@ -85,11 +101,11 @@ class Die //models one single dice cube
     }
     this.display=display;
   }
-  void roll()
+  public void roll()
   {
     this.display = (int)(Math.random()*6)+1;
   }
-  void show()
+  public void show()
   {
     noStroke();
     fill(this.rpaint,0,this.bpaint);
@@ -147,3 +163,12 @@ class Die //models one single dice cube
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
